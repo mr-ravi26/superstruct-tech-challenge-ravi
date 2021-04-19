5ea29bea8d529d7f7abd123894847e0ecdf3688e
David Guttman <david@davidguttman.com>
2021-04-18 22:22:44 -0700

    feat: add base64 endpoint

--- test.js
+++ test.js
@@ -20,7 +20,15 @@ tape('should respond user-agent', (t) => {
   jsonist.get(`${urlBase}/user-agent`, opts, (err, body) => {
     if (err) t.error(err)
 
-    t.equal(body.ua, 'tape')
+    t.equal(body.ua, 'tape')    t.end()
+  })
+})
+
+tape('should respond b64', (t) => {
+  jsonist.get(`${urlBase}/b64/hello`, (err, body) => {
+    if (err) t.error(err)
+
+    t.equal(body.b64, 'aGVsbG8=')
     t.end()
   })
 })
